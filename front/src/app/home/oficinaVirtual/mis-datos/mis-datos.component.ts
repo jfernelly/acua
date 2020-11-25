33d4d9b0e6773b04f64b193b81289ab043dd7329
Jorge F Rincon <jorgefr_inguitar@hotmail.com>
2020-11-24 19:58:12 -0500

    3 datos

--- front/src/app/home/oficinaVirtual/mis-datos/mis-datos.component.ts
+++ front/src/app/home/oficinaVirtual/mis-datos/mis-datos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DatosService } from '../../../service/datos.service';
+import { AuthService } from '../../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mis-datos',
@@ -9,17 +10,52 @@ import { DatosService } from '../../../service/datos.service';
 export class MisDatosComponent implements OnInit {
 
   constructor(
-     
+     private auth: AuthService, 
+     private router: Router
     /* private usuario: DatosService, */
   ) { }
+//Datos obtenidos desde el HTML
+  actualizarUsuario = {
+      "nombre": "",
+      "lastName": "",
+      "tipoDocumento": "",
+      "numeroDocumento": "",
+      "departamento": "",
+      "ciudad": "",
+      "direccion": "",
+      "telefono": "",
+      "correo": "",
+      "pass": "",
+  };
 
   ngOnInit(): void {
+  }
+
+  actualizar() {
+    this.auth.registroUsuario(this.actualizarUsuario).subscribe(
+      (res) => {
+        console.log(res);
+        this.router.navigate(['/Ingreso']);
+        window.alert("Registro exito!!! Ya puedes acceder a nustra aplicación")
+      },
+      (err) => {
+        console.log(err);
+        window.alert("Tus datos están el sistema")
+      }
+    );
   }
 
 }
 
 
 
+  
+
+  
+  
+  
+
+ 
 
 
   
