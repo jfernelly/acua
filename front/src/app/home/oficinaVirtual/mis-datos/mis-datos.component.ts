601aaacdfbfbc50da175a898c40f867c505e1b43
Jorge F Rincon <jorgefr_inguitar@hotmail.com>
2020-11-24 21:21:03 -0500

    7trer

--- front/src/app/home/oficinaVirtual/mis-datos/mis-datos.component.ts
+++ front/src/app/home/oficinaVirtual/mis-datos/mis-datos.component.ts
@@ -2,60 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../service/auth.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-mis-datos',
   templateUrl: './mis-datos.component.html',
-  styleUrls: ['./mis-datos.component.css']
+  styleUrls: ['./mis-datos.component.css'],
 })
 export class MisDatosComponent implements OnInit {
-
   constructor(
-     private auth: AuthService, 
-     private router: Router
-    /* private usuario: DatosService, */
-  ) { }
-//Datos obtenidos desde el HTML
+    private auth: AuthService,
+    private router: Router
+  ) /* private usuario: DatosService, */
+  {}
+  //Datos obtenidos desde el HTML
   actualizarUsuario = {
-      "nombre": "",
-      "apellido": "",
-      "fechaNacimiento": "",
-      "genero": "",
-      "departamento": "",
-      "direccion": "",
-      "tipoVivienda":"",
-      "telefono": "",
-     
+    nombre: '',
+    apellido: '',
+    fechaNacimiento: '',
+    genero: '',
+    departamento: '',
+    ciudad: '',
+    direccion: '',
+    tipoVivienda1: '',
+    tipoVivienda2: '',
+    tipoVivienda3: '',
+    telefono: '',
   };
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   actualizar() {
     this.auth.registroUsuario(this.actualizarUsuario).subscribe(
       (res) => {
         console.log(res);
         this.router.navigate(['/Ingreso']);
-        window.alert("Registro exito!!! Ya puedes acceder a nustra aplicación")
+        window.alert('Registro exito!!! Ya puedes acceder a nustra aplicación');
       },
       (err) => {
         console.log(err);
-        window.alert("Tus datos están el sistema")
+        window.alert('Tus datos están el sistema');
       }
     );
   }
-
 }
-
-
-
-  
-
-  
-  
-  
-
- 
-
-
-  
